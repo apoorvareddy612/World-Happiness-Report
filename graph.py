9209865de18e490d62e3bc38bc58eeab3a4df8b9
Apoorva Reddy <61325120+apoorvareddy612@users.noreply.github.com>
2021-06-05 20:13:28 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def info():
+	df = pd.read_csv('/Users/apoorva/Desktop/CODES/Notebook/avg.csv')
+	
+
+	x = st.sidebar.selectbox(
+	    "X axis",
+	    tuple(df.columns.to_list())
+	)
+
+	y = st.sidebar.selectbox(
+	    "Y axis",
+	    tuple(df.columns.to_list())
+	)
+
+	sns.scatterplot(x,y,data=df,hue='Country')
+	plt.legend(bbox_to_anchor=(1.01, 1.05),borderaxespad=0)
+
+	st.pyplot()
+
+
+
+def load_page():
+	info()
+
+if __name__ == "__main__":
+	load_page()
